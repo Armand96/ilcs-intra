878c27f46322539a646cbe7c23c7e9877b263bd8
Faldi Ramadhan <indocrafter76@gmail.com>
2024-05-13 21:55:34 +0700

    dev

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -4,4 +4,5 @@
 <link href="{{ URL::asset('assets/css/metisMenu.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ URL::asset('assets/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ URL::asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('css/custom.css') }}" rel="stylesheet" type="text/css" />
 
