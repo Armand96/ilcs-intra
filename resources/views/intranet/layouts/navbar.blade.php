896ff2557cb44b591b7e35bccafd093a80853047
Faldi Ramadhan <indocrafter76@gmail.com>
2024-05-26 21:53:24 +0700

    notif

--- resources/views/intranet/layouts/navbar.blade.php
+++ resources/views/intranet/layouts/navbar.blade.php
@@ -34,13 +34,14 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                     </svg>
-                    <span class="badge badge-sm bg-red-500 border-none text-white indicator-item">8</span>
+                    <span class="badge badge-sm bg-red-500 border-none text-white indicator-item">0</span>
                 </div>
 
             </div>
             <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-60 bg-base-100 shadow">
                 <h4 class=" px-4 font-semibold my-3">Notification</h4>
-                <div class=" flex flex-col items-center justify-center mx-auto px-2 py-1 cursor-pointer border-b border-blue-950">
+                <h4 class=" px-4 text-xs text-center mb-10 mt-3">No Notification</h4>
+                <!-- <div class=" flex flex-col items-center justify-center mx-auto px-2 py-1 cursor-pointer border-b border-blue-950">
                     <p class="text-sm font-semibold text-center ">User 1 Mengomentari aspirasi Anda</p>
                     <p class="text-sm font-light text-blue-300">Klik lebih lanjut..</p>
                 </div>
@@ -51,7 +52,7 @@
                 <div class="flex flex-col mt-2 items-center mx-auto px-2 py-1 cursor-pointer border-b border-blue-950">
                     <p class="text-sm font-semibold text-center ">User 3 Mengomentari aspirasi Anda</p>
                     <p class="text-sm font-light text-blue-300">Klik lebih lanjut..</p>
-                </div>
+                </div> -->
             </div>
         </div>
 
