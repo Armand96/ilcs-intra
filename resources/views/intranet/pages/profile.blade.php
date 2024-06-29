2ddcecd5018fea7000822b6057487c60c6c097c9
Faldi Ramadhan <indocrafter76@gmail.com>
2024-06-29 21:11:55 +0700

    profile

--- resources/views/intranet/pages/profile.blade.php
+++ resources/views/intranet/pages/profile.blade.php
@@ -20,16 +20,16 @@ $user = Auth::user();
 
 
 
-    <div class="w-full flex">
-        <div class="grid w-4/6 lg:grid-cols-3">
+    <div class="w-full flex flex-col lg:flex-row">
+        <div class="grid w-full order-2 mt-4 lg:mt-0 lg:order-1 lg:w-4/6 lg:grid-cols-3">
             <div class="flex flex-col">
-                <p class="my-3 text-xs text-white">Nama</p>
+                <p class="my-3 text-sm text-white">Nama</p>
                 <div class="text-white px-4 text-sm py-2 rounded-6xl flex items-center bg-[#5d5b6c] border rounded-xl lg:w-5/6">
                     <p class="font-bold text-sm">{{ $user->name }}</p>
                 </div>
             </div>
             <div class="flex flex-col">
-                <p class="my-3 text-xs text-white">Departemen</p>
+                <p class="my-3 text-sm text-white">Departemen</p>
                 <div class="text-white px-4 text-sm py-2 rounded-6xl flex items-center bg-[#5d5b6c] border rounded-xl lg:w-4/6">
                     <p class="font-bold">{!! $user->dept == "" || $user->dept == null ? "&nbsp;" : $user->dept !!}</p>
                 </div>
@@ -41,28 +41,28 @@ $user = Auth::user();
                 </div>
             </div>
             <div class="flex flex-col">
-                <p class="my-3 text-xs text-white">Nomor Induk Pegawai</p>
+                <p class="my-3 text-sm text-white">Nomor Induk Pegawai</p>
                 <div class="text-white px-4 text-sm py-2 rounded-6xl flex items-center bg-[#5d5b6c] border rounded-xl lg:w-2/6">
                     <p class="font-bold text-sm">{{ $user->nip }}</p>
                 </div>
             </div>
             <div class="flex flex-col">
-                <p class="my-3 text-xs text-white">Jabatan</p>
+                <p class="my-3 text-sm text-white">Jabatan</p>
                 <div class="text-white px-4 text-sm py-2 rounded-6xl flex items-center bg-[#5d5b6c] border rounded-xl lg:w-4/6">
                     <p class="font-bold">{{ $user->jabatan }}</p>
                 </div>
             </div>
         </div>
 
-        <div class="flex w-2/6 flex-col justify-center items-center mt-4">
-            <img src="{{ strpos($user->image_user, 'http') !== false ? $user->image_user : url('storage/profile_picture/'.$user->image_user) }}" class="h-24 w-24 mt-3" alt="profile" onerror="this.src='{{ asset('assets/images/default-profile.png') }}'">
+        <div class="flex w-full order-1 lg:order-2 lg:w-2/6 flex-col justify-center items-center mt-4">
+            <img src="{{ strpos($user->image_user, 'http') !== false ? $user->image_user : url('storage/profile_picture/'.$user->image_user) }}" class="h-32 w-32 mt-3 rounded-full" alt="profile" onerror="this.src='{{ asset('assets/images/default-profile.png') }}'">
             <form action="{{route('update.profile')}}" class="flex flex-col gap-6" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="flex flex-col">
                     <p class="my-3 text-sm text-white">Ganti Profile</p>
                     <input name="foto" type="file" class="text-white px-4 text-sm py-2 rounded-6xl flex items-center bg-[#5d5b6c] border rounded-xl" accept="image/png, image/gif, image/jpeg"/>
                 </div>
-                <button class="bg-login-button text-white px-3 py-3 rounded-xl">update profile</button>
+                <button class="bg-login-button text-white px-3 py-2 text-sm rounded-xl">update profile</button>
             </form>
         </div>
 
