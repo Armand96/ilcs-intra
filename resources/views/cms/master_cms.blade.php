2d7a2bb7eece9d591e0017aadd6bd283e8873e4b
Faldi Ramadhan <indocrafter76@gmail.com>
2024-07-11 22:42:03 +0700

    home cms

--- resources/views/cms/master_cms.blade.php
+++ resources/views/cms/master_cms.blade.php
@@ -16,7 +16,7 @@
                 <!-- Page content here -->
                 <!-- <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label> -->
 
-                <div class="flex w-full px-14 py-6">
+                <div class="flex w-full px-14 py-6 ">
                     @yield('content')
                 </div>
 
--- resources/views/cms/pages/home.blade.php
+++ resources/views/cms/pages/home.blade.php
@@ -2,8 +2,27 @@
 
 @section('content')
 
-<div class="text-black ">
-    Welcome Admin ^~^
+<div class="flex flex-col w-full">
+    <h1 class="text-3xl text-center text-black">Selamat datang Admin</h1>
+    <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 mt-11">
+        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah User</p>
+            <p class="font-bold text-3xl text-white">100</p>
+        </div>
+
+        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah event</p>
+            <p class="font-bold text-3xl text-white">100</p>
+        </div>
+        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah news</p>
+            <p class="font-bold text-3xl text-white">100</p>
+        </div>
+        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah post employe forum hari ini</p>
+            <p class="font-bold text-3xl text-white">100</p>
+        </div>
+    </div>
 </div>
 
 @endsection
