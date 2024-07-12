133ecfffea9008077354adfe62abfe34e35d9861
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2024-07-12 18:59:48 +0700

    home cms

--- app/Http/Controllers/CMSController.php
+++ app/Http/Controllers/CMSController.php
@@ -2,12 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CalendarEvent;
+use App\Models\News;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CMSController extends Controller
 {
     public function index()
     {
-        return view('cms.pages.home');
+        $data = [
+            'user' => $this->totalUsers(),
+            'news' => $this->totalNews(),
+            'post' => $this->totalPosts(),
+            'event' => $this->totalEventsThisYear()
+        ];
+        // dd($data);
+
+        return view('cms.pages.home', compact('data'));
+    }
+
+    public function totalUsers()
+    {
+        $activeUsers = User::where('is_active', true)->count();
+        $inactiveUsers = User::where('is_active', false)->count();
+        $total = $activeUsers + $inactiveUsers;
+
+        $data = [
+            'active' => $activeUsers,
+            'inactive' => $inactiveUsers,
+            'total' => $total
+        ];
+
+        return $data;
+    }
+
+    public function totalNews()
+    {
+        $activeNews = News::where('is_active', true)->whereYear('created_at', date('Y'))->count();
+        $inactiveNews = News::where('is_active', false)->whereYear('created_at', date('Y'))->count();
+        $total = $activeNews + $inactiveNews;
+
+        $data = [
+            'active' => $activeNews,
+            'inactive' => $inactiveNews,
+            'total' => $total
+        ];
+
+        return $data;
+    }
+
+    public function totalPosts()
+    {
+        return Post::whereYear('created_at', date('Y'))->count();
+    }
+
+    public function totalEventsThisYear()
+    {
+        return CalendarEvent::whereYear('tgl_cal_event_start', date('Y'))->count();
     }
 }
--- resources/views/cms/pages/home.blade.php
+++ resources/views/cms/pages/home.blade.php
@@ -5,23 +5,24 @@
 <div class="flex flex-col w-full">
     <h1 class="text-3xl text-center text-black">Selamat datang Admin</h1>
     <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 mt-11">
-        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
-            <p class="text-xl text-white">Jumlah User</p>
-            <p class="font-bold text-3xl text-white">100</p>
-        </div>
+        <a href="{{ route('users.index') }}" class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Total jumlah user</p>
+            <p class="font-bold text-3xl text-white">{{ $data['user']['total'] }}</p>
+        </a>
 
-        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
-            <p class="text-xl text-white">Jumlah event</p>
-            <p class="font-bold text-3xl text-white">100</p>
-        </div>
-        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
-            <p class="text-xl text-white">Jumlah news</p>
-            <p class="font-bold text-3xl text-white">100</p>
-        </div>
-        <div class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
-            <p class="text-xl text-white">Jumlah post employe forum hari ini</p>
-            <p class="font-bold text-3xl text-white">100</p>
-        </div>
+        <a href="{{ route('calendars.index') }}" class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah event tahun ini</p>
+            <p class="font-bold text-3xl text-white">{{ $data['event'] }}</p>
+        </a>
+
+        <a href="{{ route('news.index') }}" class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah news tahun ini</p>
+            <p class="font-bold text-3xl text-white">{{ $data['news']['total'] }}</p>
+        </a>
+        <a href="{{ route('employee.forum') }}" class="flex flex-col items-center justify-center rounded-xl bg-gray-600 border h-32 text-center  px-4 py-3">
+            <p class="text-xl text-white">Jumlah post employe forum tahun ini</p>
+            <p class="font-bold text-3xl text-white">{{ $data['post'] }}</p>
+        </a>
     </div>
 </div>
 
